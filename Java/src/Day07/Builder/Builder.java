896fd58c94860e249094979d440c04d25162442c
tj-bu-706-21 <tj-bu-706-21@DESKTOP-5AQKN5H>
2025-03-18 18:45:19 +0900

    7일차(1)

--- Java/src/Day07/Builder/Builder.java
+++ Java/src/Day07/Builder/Builder.java
@@ -3,8 +3,8 @@ package Day07.Builder;
 public class Builder {
 
 	public static void main(String[] args) {
-		Pikachu pikachu = new Pikachu.Builder()
-									 .energy(100)
+		Pikachu pikachu = new Pikachu.Builder(50, "불", 10) // 매개변수 생성자에 들어감!
+									 .energy(100) // 요 밑에 얘네는 각각 따로 만들어진 애들한테 들어가서 반환됨
 									 .type("전기")
 									 .level(10)
 									 .build();
